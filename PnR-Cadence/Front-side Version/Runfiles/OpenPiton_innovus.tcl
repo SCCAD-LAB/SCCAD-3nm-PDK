09817d1b5aedfef4b4053e3b46cab0930634c536
Robert Huang <yhuang861@gatech.edu>
2026-05-22 20:05:53 -0400

    Remove Tcl license placeholders

--- PnR-Cadence/Front-side Version/Runfiles/OpenPiton_innovus.tcl
+++ PnR-Cadence/Front-side Version/Runfiles/OpenPiton_innovus.tcl
@@ -6,7 +6,6 @@
 # Floorplan   : 300.006 x 362.01 um (margins 2)
 # PDK family  : 3nm GAA FSPR (Front-Side Power Rail)
 # Author      : SCCAD Lab
-# License     : (placeholder -- to be set by Prof. Lim)
 #
 # Required inputs in current working directory:
 #   - tile.netlist.v                (synthesized Verilog netlist)
