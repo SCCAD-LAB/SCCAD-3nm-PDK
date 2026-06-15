56cbc365643125d7ca0919f4cd1e94472679f698
Huang <yhuang861@gatech.edu>
2026-06-15 15:09:42 -0400

    Add OpenPiton SRAM Liberty views

--- PnR-OpenROAD/Front-side Version/OpenROAD_Runfiles/design_configs/openpiton.tcl
+++ PnR-OpenROAD/Front-side Version/OpenROAD_Runfiles/design_configs/openpiton.tcl
@@ -11,8 +11,9 @@ set synth_verilog "../../../Sample Designs/PnR Sample-OpenROAD/openpiton/tile.ne
 set sdc_file "../../../Sample Designs/PnR Sample-OpenROAD/openpiton/tile.sdc"
 
 set openpiton_macro_lef_dir "../../../Sample Designs/PnR Sample-Cadence/openpiton/HARD-LEF (OpenPiton)"
+set openpiton_macro_lib_dir "../../../openpiton_mem_L3_256k/2d_hard_lib"
 set extra_lef [glob -nocomplain "$openpiton_macro_lef_dir/*.lef"]
-set extra_liberty {}
+set extra_liberty [glob -nocomplain "$openpiton_macro_lib_dir/*.lib"]
 
 set die_area {0 0 300.006 362.010}
 set core_area {1.995 2.010 298.011 360.000}
@@ -22,4 +23,3 @@ set cap_margin 20
 set global_place_density 0.60
 set macro_place_halo {1 1}
 set macro_place_channel {10 10}
-
